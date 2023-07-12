658a383f3462b1e5d620e44a4873ea3aa6ba4de4
Uche Chris <chrisuche58@yahoo.com>
2023-07-12 22:28:29 +0100

    cloud, line and curve

--- src/components/Airplane.jsx
+++ src/components/Airplane.jsx
@@ -5,13 +5,32 @@ Command: npx gltfjsx@6.2.7 public/models/airplane/model.glb
 
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+
+// rotate speed
+const HELIX_SPEED = 6;
 
 export function Airplane(props) {
   const { nodes, materials } = useGLTF('./models/airplane/model.glb')
+
+//   create its blade rotate
+  const helix = useRef()
+
+  useFrame((_state, delta) => {
+    helix.current.rotation.x += delta * HELIX_SPEED;
+  })
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.PUSHILIN_Plane_Circle000.geometry} material={materials.plane} />
-      <mesh geometry={nodes.PUSHILIN_Plane_Helix.geometry} material={materials.plane} position={[1.09, 0.23, 0]} />
+      <mesh 
+        geometry={nodes.PUSHILIN_Plane_Circle000.geometry} 
+        material={materials.plane} 
+        />
+      <mesh 
+        ref={helix}
+        geometry={nodes.PUSHILIN_Plane_Helix.geometry} 
+        material={materials.plane} 
+        position={[1.09, 0.23, 0]} 
+        />
     </group>
   )
 }
