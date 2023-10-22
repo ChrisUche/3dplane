1e0bf198713a36280d92461cf0d4c3a8780aa424
Uche Chris <chrisuche58@yahoo.com>
2023-10-22 03:15:02 +0100

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { PlayProvider } from './contexts/Play.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PlayProvider>
       <App />
+      <Analytics />
     </PlayProvider>
   </React.StrictMode>,
 )
